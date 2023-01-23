c10ff6fd94a26d38d9a9c6a11bf74de1755dffab
Crowdbotics <engineering@crowdbotics.com>
2023-01-23 17:24:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-wind-dev-58927",
+  url: "https://proud-wind-dev-58927.botics.co",
+  api: "https://proud-wind-dev-58927.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
